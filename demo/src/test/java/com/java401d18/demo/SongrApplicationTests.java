3fadabadd2b20be5c091ebf37f59fa4df664621c
Kyle Knight <knightkyle@outlook.com>
2023-07-28 01:53:07 -0700

    Upload Lab

---
+++ demo/src/test/java/com/java401d18/demo/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java401d18.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
